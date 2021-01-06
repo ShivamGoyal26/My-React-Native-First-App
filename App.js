5c36610f4dfc339e09127346c826bcf5c54972a8
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-01-06 11:41:41 +0530

    Working on the deleting items

--- App.js
+++ App.js
@@ -1,41 +1,30 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, Button, View, TextInput, Text,  FlatList} from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput'
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-
-  }
-
-  const addGoalHandler = () => {
-    console.log(enteredGoal);
-    setCourseGoals(currentGoals => [...currentGoals, {key: Math.random().toString(), value: enteredGoal}
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle }
     ]);
   };
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer} >
-        <TextInput placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
-   
-      <FlatList 
-      data={courseGoals} 
-      renderItem={itemData => (
-      <View  style={styles.listItem}>
-            <Text>{itemData.item.value}</Text>
-          </View>
-          )}/>
-       
+      <GoalInput onAddGoal={addGoalHandler} />
+
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => <GoalItem onDelete={
+          () => console.log("Does it work?")}
+           title={itemData.item.value} />
+
+        } />
+
     </View>
   );
 }
@@ -44,24 +33,7 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50,
   },
-  input: {
-    width: '80%',
-    borderBottomColor: "black",
-    borderBottomWidth: 1,
-    padding: 10
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  listItem: {
-    padding: 10,
-    backgroundColor: '#ccc',
-    borderColor: "black",
-    borderWidth: 1,
-    marginVertical: 10,
-  }
+
 });
 
 
